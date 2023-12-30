ef40952d7da27bc978268d048c6977449def83ef
MainSkown <mainskown.contact@gmail.com>
2023-12-30 15:18:23 +0100

    Created function to save results

--- src/Utils/FileWriter.h
+++ src/Utils/FileWriter.h
@@ -6,10 +6,11 @@
 #include "../ProcessGenerator/ProcessStruct.h"
 #include <sys/types.h>
 #include <sys/stat.h>
+#include "../ProcessGenerator/PageStruct.h"
 
 class FileWriter{
 public:
-    static void SaveToFile(const char* path, const std::vector<Process>& processes){
+    static void SaveProcessesToFile(const char* path, const std::vector<Process>& processes){
         // Check if results directory exists if not create it
         struct stat info{};
         if(stat("./results", &info) != 0){
@@ -30,6 +31,31 @@ public:
         // Saving data
         for(auto process : processes){
             file << process.birthTime << ";" << process.startedTime << ";" << process.computationTime << ";" << process.endOfProcess << std::endl;
+        }
+
+        file.close();
+    }
+    static void SavePageAlgorithmResultsToFile(const char* path, const std::vector<PageAlgorithmResult> results){
+        // Check if results directory exists if not create it
+        struct stat info{};
+        if(stat("./results", &info) != 0){
+            mkdir("./results", 0777);
+        }
+
+        std::ofstream file;
+        file.open(path, std::ios::out | std::ios::trunc);
+
+        if(!file.is_open()){
+            std::cout << "Error opening file" << std::endl;
+            return;
+        }
+
+        // Naming columns
+        file << "Page faults;Page hits;Number of pages;Page hits ratio;Page faults ratio;Average lifetime" << std::endl;
+
+        // Saving data
+        for(auto result : results){
+            file << result;
         }
 
         file.close();
