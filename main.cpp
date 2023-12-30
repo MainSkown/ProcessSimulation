d160ccee760335bbdcb8cdb4a1ae4102fc917732
MainSkown <mainskown.contact@gmail.com>
2023-12-30 15:19:06 +0100

    Finished implementation of page algorithms

--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@
 #include "src/SJF/SJFManager.h"
 #include "src/Utils/ProcessCreator.h"
 #include "src/Utils/FileWriter.h"
+#include "src/FIFO/FIFOManager.h"
+#include "src/LRU/LRUManager.h"
+#include "src/Utils/RandomStringGenerator.h"
 
 using namespace std;
 
@@ -10,28 +13,47 @@ using namespace std;
 
 int main() {
     auto FCFS = ProcessCreator::UserCreateSimulations("FCFS");
-    for(auto x : FCFS){
-        for(int i = 0; i < x.second; i++){
+    for (auto x: FCFS) {
+        for (int i = 0; i < x.second; i++) {
             stringstream ss;
             ss << "FCFS_" << x.first << "_" << i + 1;
             stringstream filepath;
             filepath << FILE_PATH_DIRECTORY;
             filepath << ss.str();
             filepath << ".csv";
-            FileWriter::SaveToFile(filepath.str().c_str(), FCFSManager::CreateSimulation(x.first, ss.str()));
+            FileWriter::SaveProcessesToFile(filepath.str().c_str(), FCFSManager::CreateSimulation(x.first, ss.str()));
         }
     }
 
     auto SJF = ProcessCreator::UserCreateSimulations("SJF");
-    for(auto x : SJF){
-        for(int i = 0; i < x.second; i++){
+    for (auto x: SJF) {
+        for (int i = 0; i < x.second; i++) {
             stringstream ss;
             ss << "SJF_" << x.first << "_" << i + 1;
             stringstream filepath;
             filepath << FILE_PATH_DIRECTORY;
             filepath << ss.str();
             filepath << ".csv";
-            FileWriter::SaveToFile(filepath.str().c_str(), SJFManager::CreateSimulation(x.first, ss.str()));
+            FileWriter::SaveProcessesToFile(filepath.str().c_str(), SJFManager::CreateSimulation(x.first, ss.str()));
+        }
+    }
+
+    vector<PageAlgorithmResult> FIFO;
+    vector<PageAlgorithmResult> LRU;
+
+    if (!Config::getConfig().randomizeString) {
+        FIFO.emplace_back(FIFOManager::CreateSimulation(Config::getConfig().referenceString, "FIFO_ReferenceString"));
+        LRU.emplace_back(LRUManager::CreateSimulation(Config::getConfig().referenceString, "LRU_ReferenceString"));
+    } else {
+        for (int i = 0; i < Config::getConfig().simulationsCount; i++) {
+            stringstream ss;
+            ss << "FIFO_" << i + 1;
+            FIFO.emplace_back(FIFOManager::CreateSimulation(
+                    RandomStringGenerator::GenerateRandomString(Config::getConfig().randomizeLength), ss.str()));
+            ss.str("");
+            ss << "LRU_" << i + 1;
+            LRU.emplace_back(LRUManager::CreateSimulation(
+                    RandomStringGenerator::GenerateRandomString(Config::getConfig().randomizeLength), ss.str()));
         }
     }
 
